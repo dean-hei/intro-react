ad1aef8d4d06170d0d072a8a353301c16895a42e
deanjheikkila <deanjheikkila@gmail.com>
2020-03-03 21:37:46 -0800

    updated post.js

--- src/Post.js
+++ src/Post.js
@@ -1,59 +1,80 @@
 import React, {Component} from "react";
+import './App.css';
 
 let elems = {
-    header: "Dinosaurs are awesome",
-    author: "Steathly Stegosaurus",
-    body: "Check out this body property",
-    comment: "First!!"
-  };
-  
-  class PostTitle extends Component {
-    render() {
-      return <h1>{this.props.header}</h1>;
-    }
-  }
-  
-  class Author extends Component {
-    render() {
-      return (
-            <p>by {this.props.author}</p>
-        );
-    }
-  }
-  
-  class Content extends Component {
-    render() {
-      return (
-        <p>{this.props.body}</p>
-      );
-    }
-  }
-  
-  class Comments extends Component {
-      render () {
-          return (
-              <div>
-                  <h2>Comments:</h2>
-                  <p>{this.props.comment}</p>
-              </div>
-          )
-      }
-  }
+  header: 'Dinos be dinoing',
+  author: 'Pterodactyl',
+  content: 'Listen up people!!'
+}
 
-  class Page extends Component {
-      render () {
-          return (
-              <div>
-                  <PostTitle header={elems.header} />
-                  <Author author={elems.author} />
-                  <Content body={elems.body} />
-                  <Comments comment={elems.comment} />
-              </div>
-          )
-      }
+let commentStuff = [
+  {
+    commentHeader: 'Rawr',
+    commentBody: 'Dinobama says hi!'
+  },
+  {
+    commentHeader: 'Radasaurus',
+    commentBody: 'RAWR XD'
   }
-  
-  export default function Post() {
-    return <Page />;
+]
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.header}</h1>
+        <h3>{this.props.author}</h3>
+      </div>
+    );
   }
+}
+
+class Content extends Component {
+  render () {
+    return (
+      <p>{this.props.content}</p>
+    );
+  }
+}
+
+class Comment extends Component {
+  render () {
+    return (
+      <div>
+        <h3>{this.props.commentHeader}</h3>
+        <p>{this.props.commentBody}</p>
+        <input type="text" name="newContent" placeholder="Type something"
+          onChange={(e) => this.addBody(e)}
+        />
+        <button onClick={(e) => this.changeBody(e)}>Change body</button>
+      </div>
+    )
+  }
+}
+
+class CommentPost extends Component {
+  render () {
+    let comments = [];
+    commentStuff.forEach((comment, index) => {
+      comments.push(<Comment commentHeader={comment.commentHeader} commentBody={comment.commentBody} />);
+    })
+    return (
+      <div>
+        {comments}
+      </div>
+    )
+  }
+}
+
+export default function App() {
   
+  return (
+    <div>
+      <Header header={elems.header} author={elems.author} />
+      <Content content={elems.content} />
+      <CommentPost />
+    </div>
+  )
+}
+
+
